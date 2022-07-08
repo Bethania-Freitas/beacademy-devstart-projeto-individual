ba11c9203adb7711e021a4aa12c00b2c22071aea
= <bethaniafreitas@yahoo.com.br>
2022-07-08 16:07:48 -0300

    navbar

--- EnsaioVirtual/resources/views/_partials/head.blade.php
+++ EnsaioVirtual/resources/views/_partials/head.blade.php
@@ -8,4 +8,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('show.index') }}">Shows </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('rehearsal.index') }}">Ensaios </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('setlist.index') }}">Setlist </a>
+      </li>
+  </div>
+</nav>
+    <br>
     @yield('body')
