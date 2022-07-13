df84ec1a78750c51dcf2da99c07e19e49c9fe50e
= <bethaniafreitas@yahoo.com.br>
2022-07-12 23:36:29 -0300

    debugando o vita

--- EnsaioVirtual/resources/views/_partials/head.blade.php
+++ EnsaioVirtual/resources/views/_partials/head.blade.php
@@ -6,24 +6,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
+    </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('show.index') }}">Shows </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('rehearsal.index') }}">Ensaios </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('setlist.index') }}">Setlist </a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('users.index') }}">Usuários </a>
-      </li>
-  </div>
-</nav>
+  <div class="container w-75 p-3">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="container">
+          <div class="row">
+            <div class="col-10">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('show.index') }}">Shows </a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('rehearsal.index') }}">Ensaios </a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('setlist.index') }}">Setlist </a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('users.index') }}">Usuários </a>
+                </li>
+              </ul>  
+            </div>  
+            <div class="col-2">
+              <ul class="navbar-nav mr-auto">
+                @if(Auth::user())
+                  <li class="nav-item">
+                    <a class="nav-link" href="">{{ Auth::user()->name }}</a>
+                  </li>
+                  <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-outline-dark" >Sair</button>
+                  </form>
+                @else
+                  <li class="nav-item">
+                    <a class="nav-link"  href="{{ route('login') }}">Entrar</a>
+                  </li>
+                  <li>
+                    <a class="nav-link" href="{{ route('register') }}">Cadastrar</a>
+                  </li>
+                @endif
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
     <br>
-    @yield('body')
+   @yield('body')
+  </div>
+</script>
+</body>
--- EnsaioVirtual/routes/web.php
+++ EnsaioVirtual/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RehearsalController;
 use App\Http\Controllers\ShowController;
@@ -7,38 +8,48 @@ use App\Http\Controllers\SetlistController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\UserController;
 
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::post('/user', [UserController::class, 'store'])->name('users.store');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+require __DIR__.'/auth.php';
 
-Route::delete('/show/{id}', [ShowController::class, 'destroy'])->name('show.destroy');
-Route::get('/show/{id}/edit', [ShowController::class, 'edit'])->name('show.edit');
-Route::put('/show/{id}', [ShowController::class, 'update'])->name('show.update');
-Route::get('/show', [\App\Http\Controllers\ShowController::class, 'index'])->name('show.index');
-Route::post('/show', [\App\Http\Controllers\ShowController::class, 'store'])->name('show.store');
-Route::get('/show/create', [\App\Http\Controllers\ShowController::class, 'create'])->name('show.create');
-Route::get('/show/{id}', [\App\Http\Controllers\ShowController::class, 'show'])->name('show.show');
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::delete('/setlist/{id}', [SetlistController::class, 'destroy'])->name('setlist.destroy');
-Route::get('/setlist/{id}/edit', [SetlistController::class, 'edit'])->name('setlist.edit');
-Route::put('/setlist/{id}', [SetlistController::class, 'update'])->name('setlist.update');
-Route::get('/setlist', [\App\Http\Controllers\SetlistController::class, 'index'])->name('setlist.index');
-Route::post('/setlist', [\App\Http\Controllers\SetlistController::class, 'store'])->name('setlist.store');
-Route::get('/setlist/create', [\App\Http\Controllers\SetlistController::class, 'create'])->name('setlist.create');
-Route::get('/setlist/{id}', [\App\Http\Controllers\SetlistController::class, 'show'])->name('setlist.show');
+Route::middleware(['auth'])->group(function() {
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::post('/user', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    
+    Route::delete('/show/{id}', [ShowController::class, 'destroy'])->name('show.destroy');
+    Route::get('/show/{id}/edit', [ShowController::class, 'edit'])->name('show.edit');
+    Route::put('/show/{id}', [ShowController::class, 'update'])->name('show.update');
+    Route::get('/show', [\App\Http\Controllers\ShowController::class, 'index'])->name('show.index');
+    Route::post('/show', [\App\Http\Controllers\ShowController::class, 'store'])->name('show.store');
+    Route::get('/show/create', [\App\Http\Controllers\ShowController::class, 'create'])->name('show.create');
+    Route::get('/show/{id}', [\App\Http\Controllers\ShowController::class, 'show'])->name('show.show');
+    
+    Route::delete('/setlist/{id}', [SetlistController::class, 'destroy'])->name('setlist.destroy');
+    Route::get('/setlist/{id}/edit', [SetlistController::class, 'edit'])->name('setlist.edit');
+    Route::put('/setlist/{id}', [SetlistController::class, 'update'])->name('setlist.update');
+    Route::get('/setlist', [\App\Http\Controllers\SetlistController::class, 'index'])->name('setlist.index');
+    Route::post('/setlist', [\App\Http\Controllers\SetlistController::class, 'store'])->name('setlist.store');
+    Route::get('/setlist/create', [\App\Http\Controllers\SetlistController::class, 'create'])->name('setlist.create');
+    Route::get('/setlist/{id}', [\App\Http\Controllers\SetlistController::class, 'show'])->name('setlist.show');
+    
+    Route::delete('/rehearsal/{id}', [RehearsalController::class, 'destroy'])->name('rehearsal.destroy');
+    Route::get('/rehearsal/{id}/edit', [RehearsalController::class, 'edit'])->name('rehearsal.edit');
+    Route::put('/rehearsal/{id}', [RehearsalController::class, 'update'])->name('rehearsal.update');
+    Route::get('/rehearsal', [\App\Http\Controllers\RehearsalController::class, 'index'])->name('rehearsal.index');
+    Route::post('/rehearsal', [\App\Http\Controllers\RehearsalController::class, 'store'])->name('rehearsal.store');
+    Route::get('/rehearsal/create', [\App\Http\Controllers\RehearsalController::class, 'create'])->name('rehearsal.create');
+    Route::get('/rehearsal/{id}', [\App\Http\Controllers\RehearsalController::class, 'show'])->name('rehearsal.show');
+    
+    Route::get('file', [FileController::class, 'index'])->name('file');
+    Route::post('file', [FileController::class, 'store'])->name('file.store');
+});
 
-Route::delete('/rehearsal/{id}', [RehearsalController::class, 'destroy'])->name('rehearsal.destroy');
-Route::get('/rehearsal/{id}/edit', [RehearsalController::class, 'edit'])->name('rehearsal.edit');
-Route::put('/rehearsal/{id}', [RehearsalController::class, 'update'])->name('rehearsal.update');
-Route::get('/rehearsal', [\App\Http\Controllers\RehearsalController::class, 'index'])->name('rehearsal.index');
-Route::post('/rehearsal', [\App\Http\Controllers\RehearsalController::class, 'store'])->name('rehearsal.store');
-Route::get('/rehearsal/create', [\App\Http\Controllers\RehearsalController::class, 'create'])->name('rehearsal.create');
-Route::get('/rehearsal/{id}', [\App\Http\Controllers\RehearsalController::class, 'show'])->name('rehearsal.show');
 
 
-Route::get('file', [FileController::class, 'index'])->name('file');
-Route::post('file', [FileController::class, 'store'])->name('file.store');
