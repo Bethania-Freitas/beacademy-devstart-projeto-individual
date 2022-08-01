1e9475e55e7f036c2b3181f51a72f144ac5bab46
= <bethaniafreitas@yahoo.com.br>
2022-08-01 15:51:37 -0300

    ajustes finais

--- EnsaioVirtual/resources/views/_partials/head.blade.php
+++ EnsaioVirtual/resources/views/_partials/head.blade.php
@@ -32,9 +32,6 @@
                   <li class="nav-item active text-warning">
                     <a class="nav-link" href="{{ route('contact.index') }}">Contato </a>
                   </li>
-                  <li class="nav-item active text-warning">
-                    <a class="nav-link" href="{{ route('users.index') }}">Usuários </a>
-                  </li>
                 @if(Auth()->check() && auth()->user()->is_admin == 1)
                   <li class="nav-item active text-warning">
                     <a class="nav-link" href="{{ route('rehearsal.index') }}">Ensaios </a>
